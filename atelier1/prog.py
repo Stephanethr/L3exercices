095a1456e24742a8f1bf3d7599c0fcd677fce8bc
Thiry Stéphane <stethiry9@gmail.com>
2023-09-08 22:33:46 +0200

    fini exo 3

--- atelier1/prog.py
+++ atelier1/prog.py
@@ -1,12 +1,14 @@
 import random
 from math import *
+from datetime import date
+
 
 # EXERCICE 1
 
 
 def message_imc(imc: float) -> str:
     if imc <= 0:
-        return "erreur de la valeur"
+        return str(imc) + " erreur de la valeur"
     elif imc < 16.5:
         return str(imc) + " dénutrition ou famine"
     elif imc <= 18.5:
@@ -142,41 +144,68 @@ print(results)
 
 
 def solution_equation(a, b, c):
-    if a == 1:
-        ax2_str = "x2"
-    elif a == -1:
-        ax2_str = "-x2"
+    str_solution = ""
+
+    delta = discriminant(a, b, c)
+    x = racine_unique(a, b)
+    x1 = racine_double(a, b, delta, 1)
+    x2 = racine_double(a, b, delta, 2)
+
+    if delta < 0:
+        str_solution = (
+            "solution de l'équation : "
+            + str_equation(a, b, c)
+            + "\n pas de racine réelle"
+        )
+    elif delta == 0:
+        str_solution = (
+            "Solution de l'équation : "
+            + str_equation(a, b, c)
+            + "\n Racine unique : x = "
+            + str(x)
+        )
     else:
-        ax2_str = f"{a}x2"
+        str_solution = (
+            "Solution de l'équation : "
+            + str_equation(a, b, c)
+            + "\n Deux racines : \n x1 = "
+            + str(x1)
+            + "\n x2 = "
+            + str(x2)
+        )
 
-    # Représenter le terme bx
-    if b == 0:
-        bx_str = ""
-    elif b == 1:
-        bx_str = " + x"
-    elif b == -1:
-        bx_str = " - x"
-    elif b > 0:
-        bx_str = f" + {b}x"
-    else:
-        bx_str = f" - {-b}x"
-
-    # Représenter le terme constant c
-    if c == 0:
-        c_str = ""
-    elif c > 0 and (
-        a != 0 or b != 0
-    ):  # If a or b is non zero, we might need a "+" sign
-        c_str = f" + {c}"
-    else:
-        c_str = f" {c}"
-
-    # Assembler l'équation
-    equation = f"{ax2_str}{bx_str}{c_str} = 0"
-    return equation
+    return str_solution
 
 
-# Tester la fonction avec des cas supplémentaires
-test_cases = [(2, 3, 4), (1, 0, 4), (1, -1, -1)]
-results = [str_equation(a, b, c) for a, b, c in test_cases]
-print(results)
+# test solution_equation
+def test_solution():
+    print(
+        "---------------------SOLUTION EQUATION PAS DE RACINE REELLE-----------------------------"
+    )
+    print(solution_equation(5, -4, 48))
+    print(
+        "---------------------SOLUTION EQUATION RACINE UNIQUE-----------------------------"
+    )
+    print(solution_equation(1, 1, 1))
+    print(
+        "---------------------SOLUTION EQUATION RACINE DOUBLE-----------------------------"
+    )
+    print(solution_equation(-5, -4, 48))
+
+
+test_solution()
+
+# EXERCICE 4
+
+
+def date_est_valide(jour: int, mois: int, annee: int) -> bool:
+    """_summary_
+
+    Args:
+        jour (int): _description_
+        mois (int): _description_
+        annee (int): _description_
+
+    Returns:
+        bool: _description_
+    """
