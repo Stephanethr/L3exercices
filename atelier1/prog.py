494b496b499e0a7e8ca67e7d16fcaab32a3da860
Stephanethr <stethiry9@gmail.com>
2023-09-10 14:51:21 +0200

    fini atelier 1 + exercire 3 atelier 2

--- atelier1/prog.py
+++ atelier1/prog.py
@@ -1,6 +1,6 @@
 import random
 from math import *
-from datetime import date
+from datetime import *
 
 
 # EXERCICE 1
@@ -39,8 +39,8 @@ def est_bissextile(year):
     return (year % 4 == 0 and not year % 100 == 0) or year % 400 == 0
 
 
-def test_year(nbEssai):
-    year = [4, 100, 400, 444]
+def test_year():
+    year = [4, 100, 400, 444, 2024, 2004, 1998, 2005]
     for y in year:
         if est_bissextile(y):
             print(y, " : est bissexstile")
@@ -48,7 +48,7 @@ def test_year(nbEssai):
             print(y, " : n'est pas bissextile")
 
 
-test_year(15)
+test_year()
 
 
 # EXERCIE 3
@@ -198,6 +198,7 @@ test_solution()
 # EXERCICE 4
 
 
+# Fonction pour vérifier si une date est valide
 def date_est_valide(jour: int, mois: int, annee: int) -> bool:
     """_summary_
 
@@ -209,3 +210,75 @@ def date_est_valide(jour: int, mois: int, annee: int) -> bool:
     Returns:
         bool: _description_
     """
+
+    if mois < 1 or mois > 12:
+        return False
+    if jour < 1 or jour > 31:
+        return False
+    if mois == 2:
+        if est_bissextile(annee):
+            return jour <= 29
+        else:
+            return jour <= 28
+    elif mois in [4, 6, 9, 11]:
+        return jour <= 30
+    else:
+        return True
+
+# Fonction pour saisir une date de naissance depuis le clavier
+def saisie_date_naissance() -> date:
+    while True:
+        try:
+            annee = int(input("Entrez l'année de naissance : "))
+            mois = int(input("Entrez le mois de naissance : "))
+            jour = int(input("Entrez le jour de naissance : "))
+            if date_est_valide(jour, mois, annee):
+                return datetime.date(annee, mois, jour)  # Utilisation de datetime.date() pour créer un objet date
+            else:
+                print("Date invalide. Veuillez entrer une date de naissance valide.")
+        except ValueError:
+            print("Erreur de saisie. Veuillez entrer des entiers pour l'année, le mois et le jour.")
+
+
+# Fonction pour calculer l'âge
+def age(date_naissance: date) -> int:
+    """_summary_
+
+    Args:
+        date_naissance (date): _description_
+
+    Returns:
+        int: _description_
+    """
+    aujourdhui = datetime.date.today()
+    age = aujourdhui.year - date_naissance.year - ((aujourdhui.month, aujourdhui.day) < (date_naissance.month, date_naissance.day))
+    return age
+
+# Fonction pour vérifier si une personne est majeure
+def est_majeur(date_naissance: date) -> bool:
+    """_summary_
+
+    Args:
+        date_naissance (date): _description_
+
+    Returns:
+        bool: _description_
+    """
+    return age(date_naissance) >= 18
+
+
+d = datetime.date(1998, 4, 8)
+
+print(d)
+
+# Procédure de test d'accès
+def test_acces():
+    date_naissance = saisie_date_naissance()
+    age_personne = age(date_naissance)
+    if est_majeur(date_naissance):
+        print(f"Bonjour, vous avez {age_personne} ans, Accès autorisé.")
+    else:
+        print(f"Désolé, vous avez {age_personne} ans, Accès interdit.")
+
+# Test des fonctions et procédures
+test_acces()
--- atelier2/exo2.py
+++ atelier2/exo2.py
@@ -1,4 +1,4 @@
-def position(list: list, elt: int) -> int:
+def position_v1(list: list, elt: int) -> int:
     """_summary_
 
     Args:
@@ -8,3 +8,192 @@ def position(list: list, elt: int) -> int:
     Returns:
         int: indice of elt in list
     """
+    len_list = len(list)
+    for index in range (len_list):
+        if elt == list[index]:
+            return index 
+    return -1
+    
+
+
+
+
+def position_v2(list: list, elt: int) -> int:
+    """_summary_
+
+    Args:
+        list (list): _description_
+        elt (int): _description_
+
+    Returns:
+        int: indice of elt in list
+    """
+    len_list = len(list)
+    index = 0
+    while index < len_list:
+        if elt == list[index]:
+            return index
+        index += 1
+    return -1
+
+
+
+
+list = [1,5,7,4,2,8,9]
+elt = 8
+
+print("position v1", position_v1(list, elt))
+print("position v2", position_v2(list, elt))
+
+def nb_occurences(list: list, elt: int) -> int:
+    """_summary_
+
+    Args:
+        list (list): _description_
+        elt (int): _description_
+
+    Returns:
+        int: _description_
+    """
+    occurences = 0
+
+    for number in list:
+        if elt == number:
+            occurences += 1
+    return occurences
+
+
+# test nb_occurences
+list_occurences = [1, 5, 3, 3, 6, 7, 8, 56, 4, 3, 3, 2, 2, 8, 8]
+print("test nb_occurences : " , nb_occurences(list_occurences, 3))
+
+def est_triee_v1(list: list) -> bool:
+    """_summary_
+
+    Args:
+        list (list): _description_
+
+    Returns:
+        bool: _description_
+    """
+    len_list = len(list) - 1
+    for i in range(len_list):
+        if list[i] > list[i + 1]:
+            return False
+    return True
+
+
+def est_triee_v2(list: list) -> bool:
+    """_summary_
+
+    Args:
+        list (list): _description_
+
+    Returns:
+        bool: _description_
+    """
+    len_list = len(list) - 1
+    i = 0
+
+    while i < len_list:
+        if list[i] > list[i + 1]:
+            return False
+        i += 1
+    return True
+
+# test est_triee
+
+list_triee = [1, 2, 3, 4, 5, 6, 7, 8]
+list_pas_triee = [1, 5, 3, 6, 7, 8]
+
+print("test v1 triee : ", est_triee_v1(list_triee))
+print("test v2 triee : ", est_triee_v1(list_triee))
+print("test v1 pas triee : ", est_triee_v1(list_pas_triee))
+print("test v2 pas triee : ", est_triee_v1(list_pas_triee))
+
+def position_tri(list: list, elt: int) -> int:
+    """_summary_
+
+    Args:
+        list (list): _description_
+        elt (int): _description_
+
+    Returns:
+        int: _description_
+    """
+    len_list = len(list)
+    left, right = 0, len_list - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if list[mid] == elt:
+            return mid
+        elif list[mid] < elt:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+# test position tri
+
+
+def a_repetitions(list) -> bool:
+    """_summary_
+
+    Args:
+        list (_type_): _description_
+
+    Returns:
+        bool: _description_
+    """
+    len_list = len(list)
+    list_repetition = []
+    i = 0
+    while i < len_list:
+        if list[i] not in list_repetition:
+            list_repetition.append(list[i])
+        else :
+            return True
+        i += 1
+    return False
+
+# test a_repetition
+
+print("liste avec repetition (attente true): " + str(a_repetitions(list_occurences)))
+print("liste sans repetition (attente false): " + str(a_repetitions(list_triee)))
+
+
+# EXO 3
+
+def separer(L: list) -> list:
+    """_summary_
+
+    Args:
+        L (list): _description_
+
+    Returns:
+        list: _description_
+    """
+
+    LSEP = []  # Initialisation de la liste LSEP
+    
+    # Ajout des nombres négatifs à gauche
+    for num in L:
+        if num < 0:
+            LSEP.insert(0, num)
+    
+    # Ajout des zéros au milieu
+    for num in L:
+        if num == 0:
+            LSEP.append(num)
+
+    # Ajout des nombres positifs à droite
+    for num in L:
+        if num > 0:
+            LSEP.append(num)
+    
+    return LSEP
+
+# Exemple d'utilisation
+liste = [3, 0, -2, 5, 0, -1, 4]
+resultat = separer(liste)
+print(resultat)  # [-2, -1, 0, 0, 3, 5, 4]
