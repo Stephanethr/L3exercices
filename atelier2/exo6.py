5701ba43a064be96a4c8a70404b5256372b56a01
Thiry Stéphane <stethiry9@gmail.com>
2023-09-13 02:01:46 +0200

    correction

--- atelier2/exo6.py
+++ atelier2/exo6.py
@@ -161,3 +161,138 @@ test_present(correction_present3)
 
 print("TEST CORRECTION PRESENT 4")
 test_present(correction_present4)
+
+
+def pos(L: list, e: int) -> list:
+    """_summary_
+
+    Args:
+        L (list): _description_
+        e (int): _description_
+
+    Returns:
+        list: _description_
+    """
+    len_L = len(L)
+    res = []
+    for i in range(len_L):
+        if L[i] == e:
+            res.append(i)
+    return res
+
+
+# VERSION 1
+def pos1(L, e):
+    Lres = list(L)
+    for i in range(0, len(L), 1):
+        if L[i] == e:
+            Lres += [i]
+    return Lres
+
+
+def correction_pos1(L, e):
+    Lres = []
+    len_L = len(L)
+    for i in range(0, len_L, 1):
+        if L[i] == e:
+            Lres += [i]
+    return Lres
+
+
+# VERSION 2
+def pos2(L, e):
+    Lres = list(L)
+    for i in range(0, len(L), 1):
+        if L[i] == e:
+            Lres[i] = i
+    return Lres
+
+
+def correction_pos2(L, e):
+    Lres = []
+    len_L = len(L)
+    for i in range(0, len_L, 1):
+        if L[i] == e:
+            Lres += [i]
+    return Lres
+
+
+# VERSION 3
+def pos3(L, e):
+    nb = L.count(e)
+    Lres = [0] * nb
+    for i in range(0, len(L), 1):
+        if L[i] == e:
+            Lres.append(i)
+    return Lres
+
+
+def correction_pos3(L, e):
+    nb = L.count(e)
+    Lres = [0] * nb
+    for i in range(0, len(L), 1):
+        if L[i] == e:
+            Lres.append(i)
+    return Lres
+
+
+# VERSION 4
+def pos4(L, e):
+    nb = L.count(e)
+    Lres = [0] * nb
+    j = 0
+    for i in range(0, len(L), 1):
+        if L[i] == e:
+            Lres[j] = i
+    return Lres
+
+
+def correction_pos4(L, e):
+    nb = L.count(e)
+    Lres = [0] * nb
+    j = 0
+    for i in range(0, len(L), 1):
+        if L[i] == e:
+            Lres[j] = i
+    return Lres
+
+
+def test_pos(pos: callable):
+    """_summary_
+
+    Args:
+        pos (callable): _description_
+    """
+
+    L = [3, 4, 5, 7, 2, 7]
+    e = 7
+    print("position de 7 dans la liste L : ", pos(L, e))
+    print("---------------------------")
+
+
+print("TEST POS")
+test_pos(pos)
+
+print("TEST POS 1")
+test_pos(pos1)
+
+print("TEST POS 2")
+test_pos(pos2)
+
+print("TEST POS 3")
+test_pos(pos3)
+
+print("TEST POS 4")
+test_pos(pos4)
+
+print("TEST CORRECTION POS 1")
+test_pos(correction_pos1)
+
+print("TEST CORRECTION POS 2")
+test_pos(correction_pos2)
+
+print("TEST CORRECTION POS 3")
+test_pos(correction_pos3)
+
+print("TEST CORRECTION POS 4")
+test_pos(correction_pos4)
